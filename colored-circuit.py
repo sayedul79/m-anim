785ea715eb42ec533b67bd9c121df1cd881cba8e
MD. Sayedul <50768800+sayedul79@users.noreply.github.com>
2021-11-15 10:59:28 +0600

    Update colored-circuit.py

--- colored-circuit.py
+++ colored-circuit.py
@@ -9,15 +9,22 @@ class ColoredCircuit(Scene):
             r"""\draw (0,0) to[isource, l=$I_0$, v=$V_0$] (0,3);""", 
             r"""\draw (0,3) to[short, -*] (2,3);""",
             r"""\draw (2,3) to[R=$R_1$, i>_=$I_1$] (2,0);""",
-            r"""\draw (2,3) -- (4,3) to[R=$R_2$, i>_=$I_2$] (4,0) to[short, -*] (2,0)--(0,0);"""
-            , stroke_width=3
+            r"""\draw (2,3) -- (4,3);""", 
+            r"\draw (4,3) to[R=$R_2$, i>_=$I_2$] (4,0);",
+            r"\draw (4,0) to[short, -*] (2,0)--(0,0);"
+            , stroke_width=4
             , fill_opacity=0
             , stroke_opacity=1
             , tex_environment="circuitikz"
             , tex_template=template
             
             )
-        c.set_color_by_tex_to_color_map({"I_0":RED})
+        # for cir, clr in zip(c[0,4],[RED, GREEN, BLUE, YELLOW]):
+        #     cir.set_color(clr)
+        c.set_color_by_tex_to_color_map({"I_0":RED, "R_1":YELLOW, "R_2":BLUE})
 
-        self.add(c)
+        self.play(FadeIn(c, shift=UP, target_position=ORIGIN), run_time=3)
         self.play(ApplyWave(c[0]))
+        self.play(Indicate(c[2], color=TEAL), run_time=2) 
+        self.play(Circumscribe(c[4], fade_out=True, color=BLUE))
+        self.wait(2)
